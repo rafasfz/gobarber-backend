3c8e1ec3cb61e925019d4a84791d68d4ee914bd9
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2020-12-10 00:56:54 -0300

    Created implementation for mail provider

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -31,9 +31,12 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not existis.');
     }
 
-    await this.userTokensRepository.generate(user.id);
+    const { token } = await this.userTokensRepository.generate(user.id);
 
-    this.mailProvider.sendMail(email, 'Pedido de recuperação senha recebido');
+    await this.mailProvider.sendMail(
+      email,
+      `Pedido de recuperação senha recebido: ${token}`,
+    );
   }
 }
 
