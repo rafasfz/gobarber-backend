e2d2c1e665f939fbe0308e7eeba7ab4cbb16aa67
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2020-12-21 13:23:36 -0300

    Added unavaliable days before current date in list provider month availability

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -27,7 +27,11 @@ class ListProviderMonthAvailabilityService {
     year,
   }: IRequest): Promise<IResponse> {
     const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
-      { provider_id, year, month },
+      {
+        provider_id,
+        year,
+        month,
+      },
     );
 
     const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
@@ -38,13 +42,16 @@ class ListProviderMonthAvailabilityService {
     );
 
     const availability = eachDayArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
       const appointmentsInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day;
       });
 
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
 
